98d50048e29b1979792ac3334055386fc0f5c686
yune <526014795@qq.com>
2018-07-03 20:32:12 +0800

    titlefinsh

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-  	<navbar></navbar>
+  	<navbar>
+  		<i class="iconfont icon-caidan"></i>
+  	</navbar>
   	<sidebar></sidebar>
   </div>
 </template>
 
 <script>
-export default {
+import navbar from "./components/common/navbar";
+import sidebar from "./components/common/sidebar";
 
+export default {
+	data(){
+		return{
+			isShow:false
+		}
+	},
+	components:{
+		navbar,
+		sidebar
+	}
 }
 </script>
 
 <style>
+*{
+	margin: 0px;
+	padding: 0px;
+}
 
-
+html,body{
+	height: 100%;
+}
 </style>
--- src/components/common/navbar.vue
+++ src/components/common/navbar.vue
@@ -1,5 +1,13 @@
 <template>
-	
+	<nav>
+		<div class="left">
+			<a href="#"><img src="static/image/logo.png"></a>
+		</div>
+		<div class="right">
+			<slot></slot>
+			<span>登录</span>
+		</div>
+	</nav>
 </template>
 
 <script>
@@ -8,6 +16,33 @@
 	}
 </script>
 
-<style>
+<style scoped>
+	nav{
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		width: 100%;
+		height: 50px;
+		background: #292525;
+		line-height: 50px;
+		color:white;
+		z-index: 10;
+	}
+	.left{
+		float: left;
+	}
+	.right{
+		float:right;
+		padding-right: 12px;
+	}
+	.right span{
+		padding-left: 14px;
+	}
+	nav .left a img{
+		width: 40px;
+		height:38px;
+		margin-top: 6px;
+		padding-left: 5px;
+	}
 	
 </style>
