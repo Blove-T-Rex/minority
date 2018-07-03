e716672178d45816861ed00b644dab7535220e11
mini-bear <591128914@qq.com>
2018-07-03 16:02:10 +0800

    project

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+// import Login from '../components/login'
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',//登录
+      component: Login
+    },
+    {
+      path: '/join',//注册
+      component: Jion
+    },
+    {
+      path: '/topics',//专题广场
+      component: Topics
+    },
+    {
+      path: '/topic/:id',//专题广场的详情页
+      component: Topic
+    },
+    {
+      path: '/matrix',//
+      component: Matrix
+    },
+    {
+      path: '/post/:id',////matrix的详情页
+      component: Post
+    },
+    {
+      path: '/series',//付费栏目
+      component: Series
+    },
+    {
+      path: '/series/:id',//付费栏目详情页
+      component: Series
+    },
+    {
+      path: '/tag/:id',//
+      component: Tag
+    },
+
+  ]
+})
